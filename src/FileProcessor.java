e33a98477cf9112c3b52c6c36497af6109cd6323
Karlo Magdic <karlo.magdic@gmail.com>
2021-09-21 10:10:23 +0200

    Filename with date

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 public class FileProcessor {
     public static void main(String[] args) throws IOException {
-        Scanner s = new Scanner(new File("temp_croatia.txt"));
+        Scanner s = new Scanner(new File("temp_croatia_2021-09-21.txt"));
 
         // skip first line (header)
         s.nextLine();
